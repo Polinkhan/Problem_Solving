e88388e3b688cc65b3257f44acee209b8de3dcf6
Polinkhan <polinkhan308@gmail.com>
2021-08-29 00:48:35 +0600

    sub set genarator

---
+++ SUb_Set_Genarate.java
@@ -0,0 +1,15 @@
+package Hacker_Rank;
+
+class SUb_Set_Genarate {
+    static void set(String s,int index,String temp) {
+        if(index==s.length()){
+            System.out.println(temp);
+            return;
+        }
+        set(s,index+1,temp+s.charAt(index));
+        set(s,index+1,temp);
+    }
+    public static void main(String args[]) {
+        set("123",0,"");
+    }
+}
