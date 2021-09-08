51306ea7edd07d9f42055fbd9a88edd7a177d4fc
Polinkhan <polinkhan308@gmail.com>
2021-09-09 02:33:34 +0600

    HackerRank

---
+++ Hacker_Rank/src/hacker_rank/Jumping_on_the_Clouds.java
@@ -0,0 +1,32 @@
+package hacker_rank;
+
+import java.util.Scanner;
+
+public class Jumping_on_the_Clouds {
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int k = scan.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = scan.nextInt();
+        }
+        int p = 0;
+        int energy = 100;
+        while(true){
+            p = (p+k)%n;
+            if(arr[p]==1){
+                energy-=3;
+                p+=k;
+            } else{
+                energy--;
+                p+=k;
+            }
+            p = (p+k)%n;
+            System.out.println(p+" - "+energy);
+            if(p==0) break;
+        }
+        System.out.println(energy);
+    }
+}
