991ddc3155b777b2d4876a1ed20041cec661636c
Polin Khan <55098399+Polinkhan@users.noreply.github.com>
2021-12-08 01:16:51 +0600

    Add files via upload

---
+++ Scheduling_Algorithm/out.java
@@ -0,0 +1,11 @@
+package Scheduling_Algorithm;
+
+class out {
+    public static <E> void print(E element) {
+        System.out.print(element);
+    }
+
+    public static <E> void println(E element) {
+        System.out.println(element);
+    }
+}
